48d9fa3b4eaea6f8cfff07b1f1e0279f35e8b00e
Duqowdev <liibaanosman779@gmail.com>
2024-11-06 08:14:01 +0100

    file handlin add this.

--- utils/fileHandler.js
+++ utils/fileHandler.js
@@ -1,18 +1,22 @@
-const fs = require ('fs')
-const path = require ('path')
 
-const filePath = '/.data/';
+const fs = require('fs');
+const path = require('path');
 
-exports.writeTasksToFile = (Tasks) => {
-    fs.writeFileSync(filePath, JSON.stringify(tasks))
+const filePath = './data/tasks.json';
 
+exports.writeTasksToFile = (tasks) => {
+    if (Array.isArray(tasks) && tasks.length === 0) {
+        fs.writeFileSync(filePath, '[]');
+    } else {
+        fs.writeFileSync(filePath, JSON.stringify(tasks, null, 2));
+    }
 }
 
 exports.readTasksFromFile = () => {
-    if (!fs.existSync(filePath)){
-        writeTasksToFile([])
+    if(!fs.existsSync(filePath)) {
+        this.writeTasksToFile([])
     }
 
     const data = fs.readFileSync(filePath);
-    return JSON.parse()
+    return JSON.parse(data)
 }
